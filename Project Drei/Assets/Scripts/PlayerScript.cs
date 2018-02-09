1440bc16171b86a89d1d1108a9535cc89428907a
Albert <lorenzdiz@gmail.com>
2018-02-09 21:50:07 +0800

    Project Setup
    Base Actor class for player and enemy
    Basic scene setup

---
+++ Project Drei/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : Actor {
+
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKey(KeyCode.Space) && !isJumping) {
+			Jump();
+		}
+		
+		if ( Input.GetKey(KeyCode.D)) {
+			MoveRight();
+		}
+
+		if ( Input.GetKey(KeyCode.A)) {
+			MoveLeft();
+		}
+	}
+}
