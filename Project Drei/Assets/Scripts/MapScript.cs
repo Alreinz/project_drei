e8c2bfd8304fa0c3dd36bbe1585c7e5c22165d70
Albert <lorenzdiz@gmail.com>
2018-02-11 19:04:48 +0800

    Added Map preparation script and basic tile

---
+++ Project Drei/Assets/Scripts/MapScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapScript : MonoBehaviour {
+	
+	public GameObject tileBase;
+	public int sizeX;
+	public int sizeY;
+	public float tileSize;
+
+	public Sprite[] tileSprite;
+
+	public void Start () {
+		PrepareMap();
+	} 
+
+	public void PrepareMap() {
+		for ( int i = 0; i < sizeX + 2; i++ ) {
+			for ( int j = 0; j < sizeY + 2; j++ ) {
+				GameObject tile = GameObject.Instantiate(tileBase);
+				tile.transform.parent = transform;
+				tile.transform.position = new Vector3( ( i * tileSize) - ((sizeX * tileSize) / 2), (j * tileSize) - ((sizeY * tileSize) / 2), 0);
+				Tile tileScript = tile.GetComponent<Tile>();
+				if ( i == 0 || j == 0 || i == sizeX + 1 || j == sizeY + 1) {
+					tileScript.Initialize(tileSprite[Random.Range(0, tileSprite.Length - 1)], false);
+				} else {
+					tileScript.Initialize(tileSprite[Random.Range(0, tileSprite.Length - 1)], true);
+				}
+			}
+		}
+		Debug.Log("Map Loaded");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Project Drei/Assets/Scripts/Tile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Tile : MonoBehaviour {
+	
+	private SpriteRenderer spriteRenderer;
+	private BoxCollider2D boxCollider;
+	public Sprite sprite { get { return spriteRenderer.sprite; } }
+	public float size;
+	public bool isPassable;
+
+	// Use this for initialization
+	public void Initialize ( Sprite tilesprite, bool isPassable) {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		boxCollider = GetComponent<BoxCollider2D>();
+		spriteRenderer.sprite = tilesprite;
+		size = spriteRenderer.size.x;
+		
+		if ( isPassable ) {
+			boxCollider.enabled = false;
+			this.isPassable = true;
+		} else {
+			boxCollider.enabled = true;
+			boxCollider.size = new Vector2(size, size);
+		}
+	}
+}
